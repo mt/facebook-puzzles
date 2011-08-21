5ae633d7dfa3d414d74fd6a99d7331e9f743deee
mt <mathew.thomas@gmail.com>
2011-08-21 12:55:28 -0400

    First Problem: Hoppity Hop

---
+++ hoppity-hop/hoppity.hs
@@ -0,0 +1,31 @@
+#!/usr/bin/env runhaskell
+
+module Main( main ) where
+
+import System( getArgs, getProgName )
+import System.IO
+
+hopValue :: Int -> String 
+hopValue i 
+    | i `mod` 15 == 0 = "Hop"
+    | i `mod` 3  == 0 = "Hoppity"
+    | i `mod` 5  == 0 = "Hophop"
+    | otherwise       = ""
+
+processInputFile :: String -> IO ()
+processInputFile f = do 
+    inputStr <- readFile f
+    mapM_ putStrLn $ hops $ read inputStr 
+    where hops n = filter (not . null) $ map hopValue [1..n]
+
+showUsage :: IO ()
+showUsage = getProgName >>=
+            \name -> putStrLn ( "usage: " ++  name ++ " <input_file>" )
+
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of 
+        [f] -> processInputFile f
+        _   -> showUsage
+
